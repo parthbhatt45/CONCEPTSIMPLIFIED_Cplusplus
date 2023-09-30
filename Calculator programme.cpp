a9c9bb61ffd59ad5486719ac3636441be00df294
parthbhatt45 <143693198+parthbhatt45@users.noreply.github.com>
2023-09-30 22:04:17 +0530

    Update Calculator programme.cpp

--- Calculator programme.cpp
+++ Calculator programme.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 int main() {
-  // Declare variables.
+ 
   int number1, number2;
   char operator_;
-  // Get the two numbers and the operator from the user.
+  
   cout << "Enter the first number: ";
   cin >> number1;
   cout << "Enter the second number: ";
   cin >> number2;
   cout << "Enter the operator (+, -, *, /): ";
   cin >> operator_;
-  // Switch statement to perform the operation.
+  
   switch (operator_) {
     case '+':
       cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
